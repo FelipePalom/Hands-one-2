5db972e5900850d63bec2adda465dc5e2d07c4b3
FelipePalom <161984574+FelipePalom@users.noreply.github.com>
2024-03-03 00:22:31 -0600

    Update Create.bat

--- Create/Create.bat
+++ Create/Create.bat
@@ -1,16 +1,24 @@
 @echo off
 
+rem Crear un archivo de texto llamado mytext.txt y agregar la cadena "Hola Mundo"
 echo Hola Mundo > mytext.txt
+
+rem Imprimir el contenido del archivo mytext.txt en la terminal
 type mytext.txt
 
-
+rem Crear un subdirectorio llamado backup
 mkdir backup
 
-copy mytext.txt backup
+rem Mover el archivo mytext.txt al subdirectorio backup
+move mytext.txt backup\
 
+rem Listar el contenido del subdirectorio backup
 dir backup
 
+rem Eliminar el archivo mytext.txt del subdirectorio backup
 del backup\mytext.txt
 
-rmdir backup
+rem Eliminar el subdirectorio backup
+rmdir backup /s /q
+
 
